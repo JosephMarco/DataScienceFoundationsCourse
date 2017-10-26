8a6aee03f10b1ce96ab5f1f60bf16ec8cea3303d
Marco <jmarco1@its.jnj.com>
2017-10-25 22:30:04 -0400

    updates

---
+++ Capstone/r capstone investigation.R
@@ -0,0 +1,85 @@
+capstonedata <- read.csv('capstonedata_clean.csv')
+
+any(is.na(capstonedata$sku))
+
+nrow(distinct(capstonedata)) 
+
+table(capstonedata$went_on_backorder)
+
+any(is.na(capstonedata))
+
+indx = sapply(capstonedata,is.factor)
+capstonedata[indx]= lapply(capstonedata[indx],as.character)
+capstonedata[capstonedata == "Yes"] = 1
+capstonedata[capstonedata == "No"] = 0
+capstonedata[indx]= lapply(capstonedata[indx],as.numeric)
+
+str(capstonedata)
+
+library(caret)
+library(caTools)
+
+set.seed(150)
+
+split = sample.split(capstonedata$went_on_backorder, SplitRatio = .7)
+table(split)
+
+#Define train and test sets
+BackorderTrain = subset(capstonedata, split == TRUE)
+BackorderTest = subset(capstonedata, split == FALSE)
+
+#eliminate NAs from train set
+BackorderTrain <- na.omit(BackorderTrain)
+
+#count rows of train and test 
+nrow(BackorderTrain)
+nrow(BackorderTest)
+
+#use glm function
+Backorderlog <- glm(went_on_backorder ~ lead_time + national_inv , data = BackorderTrain, family = binomial)
+
+#view summary of glm function
+summary(Backorderlog)
+
+#make predictions
+predictTrain <- predict(Backorderlog, type = "response")
+summary(predictTrain)
+
+tapply(predictTrain, BackorderTrain$went_on_backorder, mean)
+
+table(BackorderTrain$went_on_backorder, predictTrain > .2)
+
+library(ROCR)
+
+ROCRpred <- prediction(predictTrain, BackorderTrain$went_on_backorder)
+ROCRperf <- performance(ROCRpred, "tpr", "fpr")
+plot(ROCRperf)
+plot(ROCRperf, colorize = TRUE)
+
+
+model_more_predictors <- glm(went_on_backorder ~ lead_time + national_inv + forecast_3_month + sales_9_month, data = BackorderTrain, family = binomial)
+
+library(lmtest)
+anova(model_more_predictors, Backorderlog, test = "LRT")
+
+#overall Accuracy
+accuracy <- 1173561/1181502
+
+#Overall Error Rate
+error_rate < 7941/1181502
+
+#calculate sensitivity
+3/7905
+#Calculate specificity
+1173558/1173597
+
+predictTest <- predict(Backorderlog, type="response", newdata = BackorderTest)
+summary(predictTest)
+
+tapply(predictTest, BackorderTest$went_on_backorder, mean)
+
+table(BackorderTest$went_on_backorder, predictTest > .2)
+
+502955/506358
+
+
